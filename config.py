0002eefccbc87368e42b37120ebef0d508b94ac1
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-05-24 12:52:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,35 +51,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Join Below 👇 All Channel  After Click “𝐍𝐨𝐰 𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞” button....!")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",
-"<b>𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐇𝐃 𝐌𝐨𝐯𝐢𝐞𝐬 
-👉 https://t.me/+4Fxg05W56SVkOTY1
-
-𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐎𝐧𝐥𝐢𝐧𝐞 𝐌𝐨𝐯𝐢𝐞𝐬 
-👉 https://t.me/+5MMPfVCCiAU5MjU1
-
-Bollywood Hindi HD MOVIES 
-👉 https://t.me/+sIUMbwiAsIo4Mjll
-
-Tamil Telugu Malayalam Movies 🎥
-👉 https://t.me/+hJLWDbymVZsyOTk1
-
-Hollywood action movie 
-👉 https://t.me/+zHXxf4Y5ve03YThl
-
-18+ Baned Movies A Rated movie Only 🔞
-👉 https://t.me/+GwoURlq-hr1hOTc9
-
-𝐓𝐫𝐞𝐧𝐝𝐢𝐧𝐠 𝐌𝐨𝐯𝐢𝐞𝐬 𝐇𝐃 
-👉 https://t.me/+EVHwFEL-OWdlMjM1
-
-Sex Video porn video 
-👉 https://t.me/+P-wgbt_2dlU3MTM1
-
-Movies 🎥
-👉https://t.me/+LpVX0gNfgEM5ZjE1
-
-©️@ROCKERSBACKUP</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐇𝐃 𝐌𝐨𝐯𝐢𝐞𝐬\n👉 https://t.me/+4Fxg05W56SVkOTY1\n\n𝐊𝐚𝐧𝐧𝐚𝐝𝐚 𝐎𝐧𝐥𝐢𝐧𝐞 𝐌𝐨𝐯𝐢𝐞𝐬\n👉 https://t.me/+5MMPfVCCiAU5MjU1\n\nBollywood Hindi HD MOVIES\n👉 https://t.me/+sIUMbwiAsIo4Mjll\n\nTamil Telugu Malayalam Movies 🎥\n👉 https://t.me/+hJLWDbymVZsyOTk1\n\nHollywood action movie\n👉 https://t.me/+zHXxf4Y5ve03YThl\n\n18+ Baned Movies A Rated movie Only 🔞\n👉 https://t.me/+GwoURlq-hr1hOTc9\n\n𝐓𝐫𝐞𝐧𝐝𝐢𝐧𝐠 𝐌𝐨𝐯𝐢𝐞𝐬 𝐇𝐃\n👉 https://t.me/+EVHwFEL-OWdlMjM1\n\nSex Video porn video\n👉 https://t.me/+P-wgbt_2dlU3MTM1\n\nMovies 🎥\n👉https://t.me/+LpVX0gNfgEM5ZjE1\n\n©️@ROCKERSBACKUP</b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
