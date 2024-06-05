f05293130bb356301220da31f77d8a29bde78f3d
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-06-06 00:36:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "MoneyKamalo.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "bc7bda749d2f288ffe87ce83020095f8ea90e67a")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_open_link_rockersbot/15")
 
 #force sub channel id, if you want enable force sub
